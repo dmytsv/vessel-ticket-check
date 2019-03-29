3a629aff1cad5f7fe4857e0e968aa4d7f9b721ed
dmytsv <dmitrytsuprunov@Dmitrys-Air.home>
2019-03-29 08:05:42 -0400

    Working prototype

--- index.js
+++ index.js
@@ -6,51 +6,41 @@ const url =
 const checkStr =
   "We are excited to plan your visit to Vessel. Tickets for the next two weeks are fully reserved. However, every morning at 8 am additional same-day tickets are made available, as well as tickets for an additional day as part of the new two-week ticketing window. Please check back regularly at 8am to plan your visit. We look forward to welcoming you to Hudson Yards soon.";
 const waitTime = 101;
-const timeoutTime = 1000 * 60 * 5;
+const timeoutTime = 1000 * 60 * 15;
 
 /** Helper functions */
 const waitNumberOfMilliseconds = async timeToWait => {
-  const timer = await new Promise(resolve => {
+  await new Promise(resolve => {
     setTimeout(() => resolve(true), timeToWait);
   });
-
-  return timer;
+  return;
 };
 
 const openBrowser = async url => {
   // Opens the url in the default browser
-  await open(url);
+  //   await open(url);
 
   // Specify the app to open in
   await open(url, { app: "google chrome" });
 };
 
-const $log = (...args) => {
-  let mainColor = "blue";
-
-  let logObj = args.reduce(
-    (logObj, arg) => ({
-      log: logObj.log + `%c${arg}`,
-      style: [
-        ...logObj.style,
-        `color: ${mainColor};
-        font-weight: bold; 
-        font-size: 10px;`
-      ]
-    }),
-    { log: "", style: [] }
-  );
-  console.log(logObj.log, ...logObj.style);
-};
-
-const getTextFromPtag = async () => {
+const getTextFromPTag = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
   //   check if selling page is active
   const text = await page.evaluate(() => {
-    return document.querySelector(".row .ng-scope > p.ng-binding").innerText;
+    let element = document.querySelector(".row .ng-scope > p.ng-binding");
+    if (element) {
+      return element.innerText;
+    } else {
+      let input = document.querySelector(".reminder-form input");
+      let button = document.querySelector(".reminder-form button");
+      input.value = "mrcuppyesq@gmail.com";
+      button.click();
+      return "";
+    }
   });
 
   console.log("Test: ", text === checkStr);
@@ -63,9 +53,10 @@ let index = 0;
 const maxIndex = Math.floor(timeoutTime / waitTime);
 
 const pageCheckRecur = async waitTime => {
-  let text = await getTextFromPtag();
+  let text = await getTextFromPTag();
   await waitNumberOfMilliseconds(waitTime);
   index++;
+
   if (index > maxIndex) {
     console.log("iteration N ", index, " Timeout");
     return;
